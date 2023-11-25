22abf022c880b2d36cd792e7dfc5585d4f67dd89
Arrari <arlen200@mail.ru>
2023-11-25 12:21:41 +0500

    First Implementation

---
+++ src/main/java/Cat.java
@@ -0,0 +1,19 @@
+public class Cat {
+    private String message;
+
+    public Cat() {
+
+    }
+
+    public Cat(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
